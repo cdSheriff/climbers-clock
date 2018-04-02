9de2185dfbb39b14fe4bf6c5cbffa57dfe437c05
Colin Dess <pcolind@gmail.com>
2018-04-02 13:33:46 -0700

    Add files via upload
    did a lot of fixing of altitude, now it is wrapped in tries and catches so no error codes poop out. Altitude recording still wrapped inside of the button/ setInterval, so it only records when asked to

--- app/index.js
+++ app/index.js
@@ -42,16 +42,16 @@ function updateClock() {
 clock.ontick = () => updateClock();
 
 ///////////////////////FIRST STAB MAKE ALTITUDE///////////////////////////////
-try {
-  let stats = fs.statSync('altitude.txt');
-}
-catch (e) {
-  let altitudeLog = [];
-  for (let i = 0; i < 51; ++i) {
-    altitudeLog[i] = '125';
-  };
-  fs.writeFileSync("altitude.txt", JSON.stringify(altitudeLog), "utf-8");
-}
+// try {
+//   let stats = fs.statSync('altitude.txt');
+// }
+// catch (e) {
+//   let altitudeLog = [125];
+//   // for (let i = 0; i < 49; ++i) {
+//   //   altitudeLog[i] = '125';
+//   // };
+//   fs.writeFileSync("altitude.txt", JSON.stringify(altitudeLog), "utf-8");
+// }
 
 ////////////////////////BUTTONS//////////////////////////////
 var myTimer;
@@ -75,9 +75,11 @@ btnWeather.onactivate = function(evt) {
 
 let btnAlt = document.getElementById("btn-br");
 btnAlt.onactivate = function(evt) {
+  btnAlt.style.stroke = 'none'
   clearDisplay();
   easyAlt();
-  myTimer = setInterval(fsAlt, 300000);
+  fsAlt();
+  myTimer = setInterval(fsAlt, 60000);
 };
 
 let btnTime = document.getElementById("btn-bl");
@@ -85,54 +87,6 @@ btnTime.onactivate = function(evt) {
   clearForClock();
 };
 /////////////////////ALTITUDE//////////////////////////////
-function refreshAlt() {
-  var altitude = (1-((bar.pressure/100)/1013.25)**0.190284)*145366.45;
-  // displayData0.text = `${Math.round(altitude)} feet`;
-  displayData0.text = ``;
-  displayData1.text = ``;
-  displayData2.text = ``;
-  label.text = `${Math.round(altitude)} feet`;
-  altitudeCollection.push(Math.round(altitude));
-  let altitudeLog = altGraph(altitudeCollection);
-  console.log('crunched version: ' + altitudeLog);
-  let x = (altitudeLog.length * 2) + 72;
-  let y = altitudeLog[altitudeLog.length-1];
-  // dot.cx = (altitudeLog.length * 2) + 72;
-  // dot.cy = altitudeLog[altitudeLog.length-1];
-  // console.log('x, y: ' + x + ' ' + y)
-};
-
-function testAlt() {
-  let k = 0;
-  let altitudeLog = [125];
-  for (let i=0; i<50;i++) {
-    altitudeLog.push(i+altitudeLog[0]+1);
-  };
-  // for (let i=0;i<51;i++) {
-  //   let y = Math.floor(Math.random()*112)+69;
-  //   alitutudeLog.push(y);
-  // };
-  dots.forEach(function(dot) {
-    dot.style.visibility = "visible";
-    // console.log(k);
-    dot.cy = altitudeLog[k];
-    k++;
-  });
-}
-
-function doubleTestAlt() {
-  try {
-    let stats = fs.statSync('altitude.txt');
-    console.log('ok... testing...');
-    console.log("File size: " + stats.size + " bytes");
-    let altitudeLog = JSON.parse(fs.readFileSync('altitude.txt', 'utf-8'));
-    console.log('this is what I got!', altitudeLog[0])
-  }
-  catch (e) {
-    console.log('jokes on you! Ha! :,(');
-  }
-}
-
 function easyAlt() {
   dots.forEach(function(dot) {
     dot.style.visibility = "visible";
@@ -141,25 +95,56 @@ function easyAlt() {
   displayData0.text = ``;
   displayData1.text = ``;
   displayData2.text = ``;
-  label.text = `${Math.round(altitude)} feet`;
+  label.text = `est. ${Math.round(altitude)} feet`;
 }
 
 function fsAlt() {
   let altitude = Math.round((1-((bar.pressure/100)/1013.25)**0.190284)*145366.45);
-  let altitudeLog = JSON.parse(fs.readFileSync('altitude.txt', 'utf-8'));
-  console.log(altitudeLog);
-  altitudeLog.shift();
-  altitudeLog.push(altitude);
-  let k = 0;
-  console.log(typeof altitudeLog[0]);
-  console.log('at [0] we have: ' + altitudeLog[0]);
-  dots.forEach(function(dot) {
-    console.log(typeof altitudeLog[k]);
-    dot.cy = parseInt(altitudeLog[k]);
-    k++;
-  });
-  fs.writeFileSync("altitude.txt", JSON.stringify(altitudeLog), "utf-8");
+  label.text = `est. ${Math.round(altitude)} feet`;
+  try {
+    let altitudeLog = JSON.parse(fs.readFileSync('altitude.txt', 'utf-8'));
+    if (altitudeLog.length == 0){
+        altitudeLog = [altitude]
+    } else if (altitudeLog.length < 51) {
+      altitudeLog.push(altitude);
+    } else {
+      altitudeLog.shift();
+      altitudeLog.push(altitude);
+    }
+    fs.writeFileSync("altitude.txt", JSON.stringify(altitudeLog), "utf-8");
+    console.log(altitudeLog.length)
+  } catch (e) {
+    let altitudeLog = [altitude];
+    fs.writeFileSync("altitude.txt", JSON.stringify(altitudeLog), "utf-8");
+  }
+  let altitudeOut = lineArt(altitudeLog)
+  // console.log(altitudeOut.length)
+  for (let i = 0; i < altitudeLog.length; i++) {
+    // console.log(altitudeOut[i])
+    dots[i].cy = altitudeOut[i]
+  }
 };
+
+function lineArt(altitudeIn) {
+  console.log(altitudeIn)
+  // console.log(Math.max(...altitudeIn))
+  // console.log(Math.min(...altitudeIn))
+  let scale = Math.ceil((Math.max(...altitudeIn) - Math.min(...altitudeIn))/100)
+  console.log(scale)
+  if (scale == 0) {scale++}
+  let delt = []
+  for (let i = 0; i < altitudeIn.length; i++) {
+    // if (altitudeIn[i] == 125) {
+    //   delt[i] = 125
+    //   console.log('found it' + delt[i])
+    // } else {
+    delt[i] = 125 + Math.round((altitudeIn[0] - altitudeIn[i])/scale)
+      console.log(delt[i])
+    // }
+  }
+  console.log(delt)
+  return delt
+}
 /////////////////WEATHER///////////////////////////////////////////
 function refreshWeather(forecast) {
   label.text = `weather`;
